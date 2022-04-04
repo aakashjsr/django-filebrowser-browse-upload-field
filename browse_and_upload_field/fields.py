221759a94db31f1256ea7f88930b3b867a716d3e
Aakash Kumar Das <aakashjsr@gmail.com>
2022-04-05 00:33:14 +0530

    Update fields.py

--- browse_and_upload_field/fields.py
+++ browse_and_upload_field/fields.py
@@ -17,7 +17,7 @@ from django.forms.widgets import Input
 from django.template.loader import render_to_string
 from django.utils.encoding import smart_str as smart_text
 from django.utils.six import with_metaclass
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.admin.templatetags.admin_static import static
 from django.contrib.admin.options import FORMFIELD_FOR_DBFIELD_DEFAULTS
 
